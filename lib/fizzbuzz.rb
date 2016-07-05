37096856009b6e2d8482e92470d218fac2862cae
Giancarlo Fabrizi <gfabrizi@Giancarlos-Air.lan>
2016-07-05 08:46:19 +0200

    git push test#ignore

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(n)
-  fizz = (n % 3 == 0)
-  buzz = (n % 5 == 0)
-  fizz && buzz ? 'fizzbuzz' : fizz ? 'fizz' : buzz ? 'buzz' : n   
-  end
+  fizz = n % 3 == 0
+  buzz = n % 5 == 0
+  fizz && buzz ? 'fizzbuzz' : fizz ? 'fizz' : buzz ? 'buzz' : n
+end
