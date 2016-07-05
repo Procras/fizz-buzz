80e392afca77222d2cad15792b36cc2c3339e8ac
Giancarlo Fabrizi <gfabrizi@Giancarlos-Air.lan>
2016-07-05 08:21:12 +0200

    if/elsif refactor

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(n)
-  fizz = (n % 3 == 0)
-  buzz = (n % 5 == 0)
-  case
-  when fizz && buzz then 'fizzbuzz'
-  when fizz then 'fizz'
-  when buzz then 'buzz'
-  else n
+  if n % 3 == 0 && n % 5 == 0
+    'fizzbuzz'
+  elsif n % 3 == 0
+    'fizz'
+  elsif n % 5 == 0
+    'buzz'
+  else
+    n
   end
 end
